430a236f584343e57bbb20a91f56941ee6a3639a
Sara Bastian <seb118@georgetown.edu>
2020-09-21 22:08:22 +0000

    Done.

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -2,6 +2,7 @@ require_relative "../lib/cruise_ship.rb"
 
 describe "#select_winner" do
   it "returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'" do 
+    
 
     passengers = {
       suite_a: "Amanda Presley", 
@@ -12,5 +13,8 @@ describe "#select_winner" do
       }
 
     expect(select_winner(passengers)).to eq("Amanda Presley")
+   
   end
 end
+
+
